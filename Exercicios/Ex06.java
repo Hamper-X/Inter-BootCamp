8b88c4e44821c21faf73d909d310e6738364f727
Hamper-X <dasmat2000@gmail.com>
2021-02-26 12:33:52 -0300

    Exercicio 6 Trabalhando com Data- Exercicio final Classe DataFormat

---
+++ Exercicios/Ex06.java
@@ -0,0 +1,31 @@
+/**
+ * Desenvolvimento Basico em Java
+ * Trabalhando com Data: Exercicio final Classe DataFormat
+*/
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.*;
+import java.util.Locale;
+import java.util.Scanner;
+import java.util.Date;
+
+public class Ex06{
+    
+    public static void teste(){
+        Date now = new Date();
+        String nowStr = DateFormat.getInstance().format(now);
+        System.out.println(nowStr);
+
+        String nowStr2 = DateFormat.getDateTimeInstance(DateFormat.LONG,DateFormat.SHORT).format(now);
+        System.out.println(nowStr2);
+
+        SimpleDateFormat form = new SimpleDateFormat("dd/MM/yyyy HH:MM:SS:MMM");
+        String nowStr3 = form.format(now);
+        System.out.println(nowStr3);
+
+    }
+
+    public static void main(String[] args) {
+        teste();
+    }
+}
